1cece497da3a8d885d18d14711fef212a750ccae
sikander <82051128+machine1337@users.noreply.github.com>
2021-06-12 11:58:36 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,10 +28,11 @@ sleep 1
 echo -e ${BLUE}"[+]Checking Go Installation\n"
 
 if [[ -z "$GOPATH" ]]; then
-  echo -e ${RED}"Go is not Installed....Plz Install it and run the script again"
+  echo -e ${RED}"[+]Go is not Installed....Plz Install it and run the script again"
+  echo -e ${CP}"[+]For Installation Plz Check my recon-automation repo pre-requisite part!"
   exit 1
   else
-  echo -e ${BLUE}"..........Go is installed.............."
+  echo -e ${BLUE}"..........Go is installed..............\n"
  fi
 echo -e ${GREEN}"[+]Installing Assetfinder\n"
 sleep 1
@@ -39,11 +40,10 @@ sleep 1
 assetfinder_checking(){
 command -v "assetfinder" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then 
-        echo "Installing assetfinder: "
         go get -u github.com/tomnomnom/assetfinder >/dev/null 2>&1
-        echo ".............assetfinder successfully installed.............."
+        echo -e ".............assetfinder successfully installed..............\n"
         else
-        echo ".......assetfinder already installed.........."
+        echo -e ".......assetfinder already installed..........\n"
     fi
 
 }
@@ -54,10 +54,10 @@ command -v "seclists" >/dev/null 2>&1
 if [[ ! -d /usr/share/seclists ]]; then 
         sudo apt update
         sudo apt install seclists >/dev/null 2>&1
-        echo "....................Seclists Successfully Installed................."
+        echo -e "....................Seclists Successfully Installed.................\n"
         
         else
-        echo ".................Seclists Already Exists................."
+        echo -e ".................Seclists Already Exists.................\n"
 fi
       
 sleep 1
@@ -66,68 +66,65 @@ amass_checking(){
 
 command -v "amass" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-         echo "Installing amass:"
          sudo apt-get install update
          sudo apt-get install amass
-         echo "................Amass successfully installed.............."
+         echo -e "................Amass successfully installed..............\n"
          else
-         echo "..........Amass is already installed.........."
+         echo -e "..........Amass is already installed..........\n"
    fi
 }
 amass_checking
 sleep 1
-echo -e ${GRAY}"[+]\nInstalling jq\n"
+echo -e ${GRAY}"[+]Installing jq\n"
 jq_checking(){
 
 command -v "jq" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-         echo "Installing jq:"
          sudo apt-get install update
          sudo apt-get install jq
-         echo ".................jq successfully installed.............."
+         echo -e ".................jq successfully installed..............\n"
          else
-         echo "...........jq is already installed.............."
+         echo -e "...........jq is already installed..............\n"
    fi
 
 }
 jq_checking
 
 sleep 1
-echo -e ${ORANGE}"[+]\nInstalling Subfinder\n"
+echo -e ${ORANGE}"[+]Installing Subfinder\n"
 subfinder_checking(){
 command -v "subfinder" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-         echo "Installing subfinder:"
-         GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-         echo "................subfinder successfully installed.............."
+         
+         GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder >/dev/null 2>&1
+         echo -e "................subfinder successfully installed..............\n"
          else
-         echo "...........subfinder is already installed............."
+         echo -e "...........subfinder is already installed.............\n"
    fi
 
 }
 subfinder_checking
 sleep 1
-echo -e ${YELLOW}"[+]\nInstalling massdns\n"
+echo -e ${YELLOW}"[+]Installing massdns\n"
 massdns_checking(){
 mkdir -p ~/tools
 command -v "massdns" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-         echo "Installing massdns.....\n\n:"
          cd ~/tools
          git clone https://github.com/blechschmidt/massdns.git
          cd massdns
           make
           cd bin
           sudo mv massdns /usr/local/bin
-          echo "............massdns installed successfully............"
+          echo -e "............massdns installed successfully............\n"
          else
-         echo "..........massdns is already installed............"
+         echo -e "..........massdns is already installed............\n"
     fi
 
 }
 massdns_checking
 sleep 1
-echo -e ${CNC}"[+]\nInstalling dnsvalidator\n"
+echo -e ${CNC}"[+]Installing dnsvalidator\n"
 dnsvalidator_installing(){
 mkdir -p ~/tools
 mkdir -p ~/tools/resolvers
@@ -143,7 +140,7 @@ if [[ $? -ne 0 ]]; then
        dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 25 -o resolvers.txt
        cat resolvers.txt | tail -n 60 > ~/tools/resolvers/resolver.txt
        else
-        echo ".......dnsvalidator already exist...!\n\n"
+        echo -e ".......dnsvalidator already exist.........\n"
 fi
 
 }
@@ -154,22 +151,22 @@ other_tools(){
 echo -e ${CPO}"[+]Installing httpx\n"
 command -v "httpx" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-         echo "Installing httpx.....\n\n:"
-        go get -v github.com/projectdiscovery/httpx/cmd/httpx
-        echo ".................httpx successfully installed.............."
+         
+        go get -v github.com/projectdiscovery/httpx/cmd/httpx >/dev/null 2>&1
+        echo -e ".................httpx successfully installed..............\n"
          else
-         echo "...............httpx is already installed............."
+         echo -e "...............httpx is already installed.............\n"
    fi
 
 sleep 1
 echo -e ${CP}"[+]Installing httprobe\n"
 command -v "httprobe" >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-         echo "Installing httprobe.....\n\n:"
-         go get -u github.com/tomnomnom/httprobe
-         echo ".............httprobe successfully installed.............."
+         
+         go get -u github.com/tomnomnom/httprobe >/dev/null 2>&1
+         echo -e".............httprobe successfully installed..............\n"
          else
-         echo "...........httprobe is already installed..............."
+         echo -e "...........httprobe is already installed...............\n"
    fi
 }
 other_tools
@@ -178,24 +175,25 @@ echo -e ${CG}"[+]Installing NMAP NSE scripts\n"
 nmap_script(){
      if [ -f /usr/share/nmap/scripts/vulners.nse ]; then
            
-         echo "...............Script already exists................"
+         echo -e "...............Script already exists................\n"
          else
          cd /usr/share/nmap/scripts
          sudo wget https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
-         echo "...............Scripts successfully installed.................."
+         echo -e "...............Scripts successfully installed..................\n"
          
     fi
 sleep 1
 echo -e ${CP}"[+] Installing vulnscan For NMAP\n"
     if [ -d /usr/share/nmap/scripts/vulscan ]; then
-               echo "....................vulnscan already exists...................."
+               echo -e "....................vulnscan already exists....................\n"
         else
         
         cd /usr/share/nmap/scripts
         sudo git clone https://github.com/scipag/vulscan.git
-        echo "......................vulnscan successfully installed................"
+        echo -e "......................vulnscan successfully installed................\n"
         
         exit 1
     fi
 }
 nmap_script
+echo -e ${BLUE}"[+] Installation Done "
